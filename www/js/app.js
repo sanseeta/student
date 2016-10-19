842131d6d10aa65cba0fb5d6fdbb9f0f83aad54b
Amulya <preetyamulya@gmail.com>
2016-10-19 19:51:01 +0530

    uploading

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+(function() {
+
+    var output = PUBNUB.$('output'), 
+        input = PUBNUB.$('input'), 
+        button = PUBNUB.$('button'),
+        avatar = PUBNUB.$('avatar'),
+        presence = PUBNUB.$('presence');
+
+    var channel = 'mchat';
+    
+    // Assign a random avatar in random color
+    avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
+
+    var p = PUBNUB.init({
+        subscribe_key: 'sub-c-dd36813a-9602-11e6-b146-0619f8945a4f',
+        publish_key:   'pub-c-b804a15f-4a92-4cdf-9679-fa530fe7979c'
+    });
+
+    p.subscribe({
+        channel  : channel,
+        callback : function(m) { 
+            output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML; 
+        },
+        presence: function(m){
+            if(m.occupancy > 1) {
+                presence.textContent = m.occupancy + ' people online';
+            } else {
+                presence.textContent = 'Nobody else is online';
+            }
+        }
+    });
+
+    p.bind('keyup', input, function(e) {
+        (e.keyCode || e.charCode) === 13 && publish()
+    });
+
+    p.bind('click', button, publish);
+
+    function publish() {
+        p.publish({
+            channel : channel, 
+            message : {avatar: avatar.className, text: input.value}, 
+            x : (input.value='')
+        });
+    }
+
+
+})();
